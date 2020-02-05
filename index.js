fd62c1052dd14b08002f1dc30258fcefcb088622
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-02-05 21:24:04 +0100

    Add test implementation

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(`It works!`);
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  console.log("It works!");
+  console.log(process.env.GITHUB_REPOSITORY);
+
+  const token = core.getInput("GITHUB_TOKEN", { required: true });
+  const octokit = new github.GitHub(token);
+
+  const { owner, repo } = process.env.GITHUB_REPOSITORY.split(" ");
+
+  const { workflows } = octokit.actions.listRepoWorkflows({
+    owner,
+    repo,
+  });
+
+  console.log(workflows);
+}
+
+run();
