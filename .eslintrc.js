95b4bd99571f86f1c9b455bf3a83a5cc7dc66daa
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-02-05 21:23:45 +0100

    Turn off console.log warnings from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
     "linebreak-style": ["error", (process.platform === "win32" ? "windows" : "unix")], // https://stackoverflow.com/q/39114446/2771889
     "import/prefer-default-export": 0, // https://stackoverflow.com/q/54245654/2771889
     "react/sort-comp": "off", // https://github.com/yannickcr/eslint-plugin-react/issues/1214
+    "no-console": "off", // console log is fine in the context of GitHub Actions
   },
 };
