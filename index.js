8de70151e9bd025e3411746ed94941b44103d5e8
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-02-10 22:04:23 +0100

    Bunch of nested loops

--- index.js
+++ index.js
@@ -29,56 +29,63 @@ async function run() {
     repoOptions
   );
 
-  for await (const workflowRun of octokit.paginate.iterator(workflowRunsRequest)
-    .data) {
-    console.log(workflowRun);
-    const { data: artifacts } = await octokit.actions.listWorkflowRunArtifacts({
-      owner,
-      repo,
-      run_id: workflowRun.id,
-    });
+  for await (const { data: workflowRuns } of octokit.paginate.iterator(
+    workflowRunsRequest
+  ).data) {
+    for await (const workflowRun of workflowRuns) {
+      console.log(workflowRun);
 
-    for await (const artifact of artifacts.artifacts) {
-      const createdAt = moment(artifact.created_at);
+      const artifactsRequest = octokit.actions.listWorkflowRunArtifacts.endpoint
+        .merge(repoOptions)
+        .merge({ run_id: workflowRun.id });
 
-      if (createdAt.isBefore(maxAge)) {
-        let shouldDelete = true;
+      for await (const { data: artifacts } of octokit.paginate.iterator(
+        artifactsRequest
+      ).data) {
+        console.log(artifacts);
+        for await (const artifact of artifacts.artifacts) {
+          const createdAt = moment(artifact.created_at);
 
-        if (skipTags) {
-          console.log("Looking for tag on sha", workflowRun.head_sha);
-          try {
-            const { data: tag } = await octokit.git.getTag({
-              owner,
-              repo,
-              tag_sha: workflowRun.head_sha,
-            });
-            if (tag) {
-              shouldDelete = false;
+          if (createdAt.isBefore(maxAge)) {
+            let shouldDelete = true;
+
+            if (skipTags) {
+              console.log("Looking for tag on sha", workflowRun.head_sha);
+              try {
+                const { data: tag } = await octokit.git.getTag({
+                  owner,
+                  repo,
+                  tag_sha: workflowRun.head_sha,
+                });
+                if (tag) {
+                  shouldDelete = false;
+                }
+                console.log(tag);
+              } catch (error) {
+                if (error.status !== 404) {
+                  throw error;
+                }
+                console.log("Tag not found for", workflowRun.head_sha);
+              }
             }
-            console.log(tag);
-          } catch (error) {
-            if (error.status !== 404) {
-              throw error;
+
+            if (shouldDelete) {
+              console.log(
+                "Deleting Artifact which was created",
+                createdAt.from(maxAge),
+                "from maximum age for Workflow Run",
+                workflowRun.id,
+                ": ",
+                artifact
+              );
+
+              await octokit.actions.deleteArtifact({
+                owner,
+                repo,
+                artifact_id: artifact.id,
+              });
             }
-            console.log("Tag not found for", workflowRun.head_sha);
           }
-        }
-
-        if (shouldDelete) {
-          console.log(
-            "Deleting Artifact which was created",
-            createdAt.from(maxAge),
-            "from maximum age for Workflow Run",
-            workflowRun.id,
-            ": ",
-            artifact
-          );
-
-          await octokit.actions.deleteArtifact({
-            owner,
-            repo,
-            artifact_id: artifact.id,
-          });
         }
       }
     }
